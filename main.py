b1dd31c85d7e34d32947df904830b998cc973371
Malynda <phoebe95.cu@gmail.com>
2023-07-21 14:03:47 +0100

    interpreter update to correct error

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+from os.path import join, dirname, realpath
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+from plyer import notification
+from plyer.utils import platform
+
+kivy.require('1.8.0')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class NotificationDemo(BoxLayout):
+
+    def do_notify(self, mode='normal'):
+        title = self.ids.notification_title.text
+        message = self.ids.notification_text.text
+        ticker = self.ids.ticker_text.text
+        kwargs = {'title': title, 'message': message, 'ticker': ticker}
+
+        if mode == 'fancy':
+            kwargs['app_name'] = "Plyer Notification Example"
+            if platform == "win":
+                kwargs['app_icon'] = join(dirname(realpath(__file__)),
+                                          'plyer-icon.ico')
+                kwargs['timeout'] = 4
+            else:
+                kwargs['app_icon'] = join(dirname(realpath(__file__)),
+                                          'plyer-icon.png')
+        elif mode == 'toast':
+            kwargs['toast'] = True
+        notification.notify(**kwargs)
 
 
-# Press the green button in the gutter to run the script.
+class NotificationDemoApp(App):
+    def build(self):
+        return NotificationDemo()
+
+    def on_pause(self):
+        return True
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    NotificationDemoApp().run()
