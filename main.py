00957deed3a2aaed91b826a941a2ebcc24f3e13d
Malynda <phoebe95.cu@gmail.com>
2023-07-26 09:00:14 +0100

    Combined notification and gps code in one kivy page wih different box layouts to test if both would work. Notification & GPS examples from plyer examples. Gotten from https://github.com/kivy/plyer/tree/master/examples

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ from kivy.uix.boxlayout import BoxLayout
 from plyer import notification
 from plyer.utils import platform
 
+from plyer import gps
+from kivy.properties import StringProperty
+from kivy.clock import mainthread
+
 kivy.require('1.8.0')
 
 
-class NotificationDemo(BoxLayout):
+class CombinedDemo(BoxLayout):
+
+    # ----------------- Notification -------------------------
 
     def do_notify(self, mode='normal'):
         title = self.ids.notification_title.text
@@ -31,14 +37,81 @@ class NotificationDemo(BoxLayout):
             kwargs['toast'] = True
         notification.notify(**kwargs)
 
+    # ----------------- GPS -------------------------
 
-class NotificationDemoApp(App):
+    def request_android_permissions(self):
+        """
+        Since API 23, Android requires permission to be requested at runtime.
+        This function requests permission and handles the response via a
+        callback.
+
+        The request will produce a popup if permissions have not already been
+        been granted, otherwise it will do nothing.
+        """
+        from android.permissions import request_permissions, Permission
+
+        def callback(permissions, results):
+            """
+            Defines the callback to be fired when runtime permission
+            has been granted or denied. This is not strictly required,
+            but added for the sake of completeness.
+            """
+            if all([res for res in results]):
+                print("callback. All permissions granted.")
+            else:
+                print("callback. Some permissions refused.")
+
+        request_permissions([Permission.ACCESS_COARSE_LOCATION,
+                             Permission.ACCESS_FINE_LOCATION], callback)
+        # # To request permissions without a callback, do:
+        # request_permissions([Permission.ACCESS_COARSE_LOCATION,
+        #                      Permission.ACCESS_FINE_LOCATION])
+
+
+class CombinedDemoApp(App):
+    gps_location = StringProperty()
+    gps_status = StringProperty('Click Start to get GPS location updates')
+
     def build(self):
-        return NotificationDemo()
+        try:
+            gps.configure(on_location=self.on_location,
+                          on_status=self.on_status)
+        except NotImplementedError:
+            import traceback
+            traceback.print_exc()
+            self.gps_status = 'GPS is not implemented for your platform'
+
+        if platform == "android":
+            print("gps.py: Android detected. Requesting permissions")
+            self.request_android_permissions()
+
+        return CombinedDemo()
+    def start(self, minTime, minDistance):
+        gps.start(minTime, minDistance)
+
+    def stop(self):
+        gps.stop()
+
+    @mainthread
+    def on_location(self, **kwargs):
+        self.gps_location = '\n'.join([
+            '{}={}'.format(k, v) for k, v in kwargs.items()])
+
+    @mainthread
+    def on_status(self, stype, status):
+        self.gps_status = 'type={}\n{}'.format(stype, status)
 
     def on_pause(self):
+        gps.stop()
         return True
+
+    def on_resume(self):
+        gps.start(1000, 0)
+        pass
+
+    # def on_pause(self):
+    #     return True
 
 
 if __name__ == '__main__':
-    NotificationDemoApp().run()
+    CombinedDemoApp().run()
